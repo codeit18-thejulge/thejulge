19d29d0f24eb356e34a4529e4e794caa0c626d46
minyoung1101 <my11612@naver.com>
2025-09-30 16:45:51 +0900

    bug: tailwindcss-preset-px-to-rem 에러 해결

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,21 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
-const pxToRem = require("tailwindcss-preset-px-to-rem");
+import * as pxToRemModule from 'tailwindcss-preset-px-to-rem';
+
+const pxToRem = pxToRemModule.default ?? pxToRemModule;
 
 const config: Config = {
   presets: [pxToRem],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
