24a5b92a1bf91a70bf6c6fea84c7843678eaf443
sanginjeong <sanginjeong07@gmail.com>
2025-10-01 03:40:24 +0900

    feat: 로그인 쿼리 구현

---
+++ src/hooks/api/user/useLoginQuery.ts
@@ -0,0 +1,16 @@
+import { instance } from "@/utils/instance";
+import { useMutation } from "@tanstack/react-query";
+import { LoginRequest, LoginResponse } from "@/types/user/signup";
+
+const postLogin = async ({ email, password }: LoginRequest): Promise<LoginResponse> => {
+  const response = await instance.post("/token", { email, password });
+  return response.data;
+};
+
+export const useLoginQuery = () => {
+  return useMutation({
+    mutationFn: postLogin,
+    onSuccess: () => {},
+    onError: () => {},
+  });
+};
--- src/types/user/signup.ts
+++ src/types/user/signup.ts
@@ -12,3 +12,27 @@ export interface SignupResponse {
   };
   links: unknown[];
 }
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  item: {
+    token: string;
+    user: {
+      item: {
+        id: string;
+        email: string;
+        type: "employee" | "employer";
+        name: string;
+        phone: string;
+        address: string;
+        bio: string;
+      };
+      href: string;
+    };
+  };
+  links: unknown[];
+}
