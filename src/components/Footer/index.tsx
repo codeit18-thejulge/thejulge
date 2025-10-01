c70163a58d57876db633173b6330129605d39345
minyoung1101 <my11612@naver.com>
2025-10-02 02:43:42 +0900

    style: 클래스네임을 하나의 문자열로 합침

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,28 +6,23 @@ import { cn } from "@/utils";
 
 const Footer = () => {
   return (
-    <footer className={cn("w-full", "flex justify-center", "bg-gray-20")}>
+    <footer className={cn("w-full flex justify-center bg-gray-20")}>
       <div
         className={cn(
-          "w-full max-w-1028",
-          "px-20 pt-32 pb-16 tablet:p-32",
-          "grid grid-cols-2 tablet:grid-cols-3 gap-40 items-center",
+          "w-full max-w-1028 px-20 pt-32 pb-16 tablet:p-32 grid grid-cols-2 tablet:grid-cols-3 gap-40 items-center",
         )}
       >
         <div
           className={cn(
-            "text-12-regular tablet:text-16-regular text-gray-50",
-            "col-span-2 tablet:col-span-1",
-            "order-last tablet:order-1",
+            "text-12-regular tablet:text-16-regular text-gray-50 col-span-2 tablet:col-span-1 order-last tablet:order-1",
           )}
         >
           ©codeit - 2023
         </div>
-        <nav aria-label="footer links" className={cn("col-span-1", "tablet:order-2")}>
+        <nav aria-label="footer links" className={cn("col-span-1 tablet:order-2")}>
           <ul
             className={cn(
-              "flex justify-start tablet:justify-center gap-30",
-              "text-14-regular tablet:text-16-regular text-gray-50",
+              "text-gray-50 flex justify-start tablet:justify-center gap-30 text-14-regular tablet:text-16-regular",
             )}
           >
             <li>
@@ -38,8 +33,8 @@ const Footer = () => {
             </li>
           </ul>
         </nav>
-        <nav aria-label="social links" className={cn("col-span-1", "tablet:order-3")}>
-          <ul className={cn("flex items-center justify-end gap-10", "text-gray-40")}>
+        <nav aria-label="social links" className={cn("col-span-1 tablet:order-3")}>
+          <ul className={cn("flex items-center justify-end gap-10 text-gray-40")}>
             <li className="hover:text-gray-50">
               <Link href="" aria-label="contact email">
                 <IcEnvelope className="w-24" />
