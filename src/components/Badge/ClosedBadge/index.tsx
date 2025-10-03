af6fbe8e50e49e5395567f8f1c2f2e61d396b9fb
kim-hyunhwa <pinpa@naver.net>
2025-10-03 18:00:41 +0900

    style : PayBadge 중괄호 추가 design : PayBadge 폰트색상 중복 제거 chore : 다른 컴포넌트는 이전 pr올릴때 커밋이 꼬인듯 하여 깃에서 변경 되었다고 알린거 같이 커밋합니다.

--- src/components/Badge/ClosedBadge/index.tsx
+++ src/components/Badge/ClosedBadge/index.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const ClosedBadge = ({ propText }: Props) => {
   const onClickDelete = () => {};
-
+  
   return (
     <div className="flex max-w-fit items-center gap-6 rounded-20 bg-red-10 px-10 py-6 text-12-regular leading-none text-primary tablet:text-14-bold">
       <span>{propText}</span>
