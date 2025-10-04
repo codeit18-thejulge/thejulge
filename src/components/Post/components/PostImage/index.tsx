6f613e275d2a992e6e1304ee7c477cc56af7c6c9
sanginjeong <sanginjeong07@gmail.com>
2025-10-05 01:20:01 +0900

    feat: 마감 완료시 이미지 구현

--- src/components/Post/components/PostImage/index.tsx
+++ src/components/Post/components/PostImage/index.tsx
@@ -3,12 +3,19 @@ import React from "react";
 
 interface Props {
   imageUrl: string;
+  closed: boolean;
 }
 
-const PostImage = ({ imageUrl }: Props) => {
+const PostImage = ({ imageUrl, closed }: Props) => {
   return (
     <div className="relative h-full w-full">
       <Image src={imageUrl} alt="가게 이미지" fill className="h-full w-full rounded-lg object-cover" />
+
+      {closed && (
+        <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black opacity-70">
+          <span className="px-4 py-2 text-28-bold text-gray-30">마감 완료</span>
+        </div>
+      )}
     </div>
   );
 };
