1fb1951ce292bf006dc2506f71c17a1af22fc772
sanginjeong <sanginjeong07@gmail.com>
2025-10-05 01:23:31 +0900

    chore: 테스트 코드 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,7 @@
-import Post from "@/components/Post";
-
 export default function Home() {
   return (
     <>
       <h1>3팀 파이팅!</h1>
-      <div className="flex gap-20">
-        <Post
-          name="진주회관"
-          id="0d5dd6f0-5306-4060-8e92-11eff1e36bed"
-          hourlyPay={15000}
-          startsAt="2023-09-14T18:00:00.000Z"
-          workhour={4}
-          description="테스트"
-          address="서울시 광진구"
-          closed={false}
-          originalHourlyPay={10000}
-          imageUrl="https://bootcamp-project-api.s3.ap-northeast-2.amazonaws.com/0-1/the-julge/1bdb43c8-ff08-4a46-81b0-7f91efced98c-jinju4.png"
-        />
-        <Post
-          name="진주회관"
-          id="0d5dd6f0-5306-4060-8e92-11eff1e36bed"
-          hourlyPay={15000}
-          startsAt="2023-09-14T18:00:00.000Z"
-          workhour={4}
-          description="테스트"
-          address="서울시 광진구"
-          closed={true}
-          originalHourlyPay={12000}
-          imageUrl="https://bootcamp-project-api.s3.ap-northeast-2.amazonaws.com/0-1/the-julge/1bdb43c8-ff08-4a46-81b0-7f91efced98c-jinju4.png"
-        />
-      </div>
     </>
   );
 }
