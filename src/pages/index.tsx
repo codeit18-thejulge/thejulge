e03b31e2a44fa930ccb0ac0a93b8920ec8316db6
sanginjeong <sanginjeong07@gmail.com>
2025-10-04 18:52:45 +0900

    feat: Post 틀 만들기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,26 +4,41 @@ export default function Home() {
   return (
     <>
       <h1>3팀 파이팅!</h1>
-      <Post
-        name="진주회관"
-        id="0d5dd6f0-5306-4060-8e92-11eff1e36bed"
-        hourlyPay={15000}
-        startsAt="2023-09-14T18:00:00.000Z"
-        workhour={4}
-        description="테스트"
-        closed={false}
-        imageUrl="https://bootcamp-project-api.s3.ap-northeast-2.amazonaws.com/0-1/the-julge/1bdb43c8-ff08-4a46-81b0-7f91efced98c-jinju4.png"
-      />
-      <Post
-        name="진주회관"
-        id="0d5dd6f0-5306-4060-8e92-11eff1e36bed"
-        hourlyPay={15000}
-        startsAt="2023-09-14T18:00:00.000Z"
-        workhour={4}
-        description="테스트"
-        closed={true}
-        imageUrl="https://bootcamp-project-api.s3.ap-northeast-2.amazonaws.com/0-1/the-julge/1bdb43c8-ff08-4a46-81b0-7f91efced98c-jinju4.png"
-      />
+      <div className="flex gap-20">
+        <Post
+          name="진주회관"
+          id="0d5dd6f0-5306-4060-8e92-11eff1e36bed"
+          hourlyPay={15000}
+          startsAt="2023-09-14T18:00:00.000Z"
+          workhour={4}
+          description="테스트"
+          address="서울시 광진구"
+          closed={false}
+          imageUrl="https://bootcamp-project-api.s3.ap-northeast-2.amazonaws.com/0-1/the-julge/1bdb43c8-ff08-4a46-81b0-7f91efced98c-jinju4.png"
+        />
+        <Post
+          name="진주회관"
+          id="0d5dd6f0-5306-4060-8e92-11eff1e36bed"
+          hourlyPay={15000}
+          startsAt="2023-09-14T18:00:00.000Z"
+          workhour={4}
+          description="테스트"
+          address="서울시 광진구"
+          closed={true}
+          imageUrl="https://bootcamp-project-api.s3.ap-northeast-2.amazonaws.com/0-1/the-julge/1bdb43c8-ff08-4a46-81b0-7f91efced98c-jinju4.png"
+        />
+        <Post
+          name="진주회관"
+          id="0d5dd6f0-5306-4060-8e92-11eff1e36bed"
+          hourlyPay={15000}
+          startsAt="2023-09-14T18:00:00.000Z"
+          workhour={4}
+          description="테스트"
+          address="서울시 광진구"
+          closed={true}
+          imageUrl="https://bootcamp-project-api.s3.ap-northeast-2.amazonaws.com/0-1/the-julge/1bdb43c8-ff08-4a46-81b0-7f91efced98c-jinju4.png"
+        />
+      </div>
     </>
   );
 }
