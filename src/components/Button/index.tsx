347861e184860d54993f06e1cacfa71308a15335
sejin5 <sejin5.dev@gmail.com>
2025-10-05 23:06:51 +0900

    feat: Button 컴포넌트 생성

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,33 @@
-const Button = () => {
-  return null;
+import { DetailedHTMLProps, ButtonHTMLAttributes } from "react";
+import { cn } from "@/utils";
+
+const BtnStyle = {
+  filled: "bg-primary text-white",
+  lined: "bg-white border-primary border-solid text-primary border",
+  disabled: "bg-gray-40 text-white",
+};
+
+type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
+
+interface BtnStatus extends ButtonProps {
+  status: keyof typeof BtnStyle;
+  children: React.ReactNode;
+}
+
+const Button = ({ status, children, disabled, ...props }: BtnStatus) => {
+  const isDisabled = disabled || status === "disabled";
+
+  return (
+    <button
+      type="button"
+      onClick={props.onClick}
+      disabled={isDisabled}
+      className={cn("inline-block w-full max-w-350 rounded-md text-14-bold", BtnStyle[status], props.className)}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
