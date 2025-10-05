5851ddba97d9466c2b0d0e93ef03b609eb502b4c
minyoung1101 <my11612@naver.com>
2025-10-06 01:55:25 +0900

    chore: test코드 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,7 @@
-import Notification from "@/components/Notification";
-import { useState } from "react";
-
 export default function Home() {
-  const [isAlertOpen, setIsAlertOpen] = useState(false);
-
   return (
     <>
       <h1>3팀 파이팅!</h1>
-      <button onClick={() => setIsAlertOpen((prev) => !prev)}>알림창 열기</button>
-      {isAlertOpen && <Notification userId="user-10" onClose={() => setIsAlertOpen(false)}></Notification>}
     </>
   );
 }
