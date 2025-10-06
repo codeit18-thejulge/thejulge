3ccdc2c890f8ad31f173ea140d319cd23f21bd06
sejin5 <sejin5.dev@gmail.com>
2025-10-06 16:11:41 +0900

    refactor: disabled 속성 수정

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,25 +4,24 @@ import { cn } from "@/utils";
 const BtnStyle = {
   filled: "bg-primary text-white",
   lined: "bg-white border-primary border-solid text-primary border",
-  disabled: "bg-gray-40 text-white",
 };
 
 type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
 
 interface BtnStatus extends ButtonProps {
-  status: keyof typeof BtnStyle;
+  status?: keyof typeof BtnStyle;
   children: React.ReactNode;
 }
 
-const Button = ({ status, children, disabled, className, ...props }: BtnStatus) => {
-  const isDisabled = disabled || status === "disabled";
-
+const Button = ({ status, children, className, ...props }: BtnStatus) => {
   return (
     <button
       type="button"
-      onClick={props.onClick}
-      disabled={isDisabled}
-      className={cn(BtnStyle[status], "inline-block w-full max-w-350 rounded-md text-14-bold", className)}
+      className={cn(
+        "inline-block w-full max-w-350 rounded-md bg-gray-40 text-14-bold text-white",
+        status && BtnStyle[status],
+        className,
+      )}
       {...props}
     >
       {children}
