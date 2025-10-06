ae668617ea0fd7b12021c9a81ce1a9dcbf3c0de8
minyoung1101 <my11612@naver.com>
2025-10-06 14:02:20 +0900

    refactor: SearchInput 구조 및 타입 개선

--- src/components/Header/SearchInput/index.tsx
+++ src/components/Header/SearchInput/index.tsx
@@ -1,34 +1,38 @@
 import IcSearch from "@/assets/svgs/ic_search.svg";
-import { cn } from "@/utils";
+import { ChangeEvent, forwardRef, InputHTMLAttributes } from "react";
 
-interface SearchInputProps {
+interface SearchInputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, "onChange"> {
   value?: string;
   onChange?: (value: string) => void;
   placeholder?: string;
 }
 
-const searchPhrase = "가게 이름으로 찾아보세요";
+const SEARCH_PLACEHOLDER = "가게 이름으로 찾아보세요";
 
-const SearchInput = ({ value = "", onChange, placeholder = searchPhrase }: SearchInputProps) => {
-  return (
-    <div
-      className={cn(
-        "bg-gray-10 rounded-10 w-full tablet:max-w-450 flex items-center p-8 tablet:p-10 gap-8 tablet:gap-10 order-3 tablet:order-2",
-      )}
-    >
-      <IcSearch className={cn("w-16 tablet:w-20")} />
-      <input
-        type="text"
-        id="search"
-        value={value}
-        onChange={(e) => onChange?.(e.target.value)}
-        placeholder={placeholder}
-        className={cn(
-          "w-full h-20 bg-inherit text-12-regular tablet:text-14-regular text-black placeholder-gray-40 outline-none",
-        )}
-      />
-    </div>
-  );
-};
+const SearchInput = forwardRef<HTMLInputElement, SearchInputProps>(
+  ({ value = "", onChange, placeholder = SEARCH_PLACEHOLDER, ...props }, ref) => {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      onChange?.(e.target.value);
+    };
+
+    return (
+      <div className="order-3 flex w-full items-center gap-8 rounded-10 bg-gray-10 p-8 tablet:order-2 tablet:max-w-450 tablet:gap-10 tablet:p-10">
+        <IcSearch className="w-16 tablet:w-20" />
+        <input
+          type="text"
+          aria-label="공고 검색"
+          value={value}
+          ref={ref}
+          onChange={handleChange}
+          placeholder={placeholder}
+          className="h-20 w-full bg-inherit text-12-regular text-black placeholder-gray-40 outline-none tablet:text-14-regular"
+          {...props}
+        />
+      </div>
+    );
+  },
+);
+
+SearchInput.displayName = "SearchInput";
 
 export default SearchInput;
