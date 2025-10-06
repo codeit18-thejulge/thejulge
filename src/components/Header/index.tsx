33a839adcb05b58e502830faf58f31a99a83c2e2
minyoung1101 <my11612@naver.com>
2025-10-06 14:20:18 +0900

    refactor: SearchInput을 내부 상태 관리로 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,25 +2,17 @@ import Logo from "@/assets/svgs/logo-md.svg";
 import Link from "next/link";
 import { cn } from "@/utils";
 import SearchInput from "./SearchInput";
-import { useState } from "react";
 import UserHeader from "./UserHeader";
 
 const Header = () => {
-  const [inputValue, setInputValue] = useState("");
-
   return (
-    <header role="banner" className={cn("w-full sticky flex justify-center")}>
-      <div className={cn("w-full max-w-1088")}>
-        <div
-          className={cn(
-            "flex flex-wrap tablet:flex-nowrap items-center justify-center gap-16 tablet:gap-30 px-20 tablet:px-32 py-10 tablet:py-15",
-          )}
-        >
+    <header role="banner" className={cn("sticky flex w-full justify-center")}>
+      <div className="w-full max-w-1088">
+        <div className="flex flex-wrap items-center justify-center gap-16 px-20 py-10 tablet:flex-nowrap tablet:gap-30 tablet:px-32 tablet:py-15">
           <Link href="/" aria-label="메인페이지로 이동" className="shrink-0">
-            <Logo className={cn("w-84 tablet:w-112")} />
+            <Logo className="w-84 tablet:w-112" />
           </Link>
-
-          <SearchInput value={inputValue} onChange={setInputValue} />
+          <SearchInput />
           <UserHeader />
         </div>
       </div>
