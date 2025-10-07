e5ececf84ce33c36356410d330442fbdd06273e1
sanginjeong <sanginjeong07@gmail.com>
2025-10-07 18:15:26 +0900

    feat: 공고 조회 hooks 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
+import { useGetNoticesQuery } from "@/hooks/api/notice/useGetNoticesQuery";
+
 export default function Home() {
+  const { data: notices } = useGetNoticesQuery({});
+
+  console.log(notices);
   return (
     <>
       <h1>3팀 파이팅!</h1>
