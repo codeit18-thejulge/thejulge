3f5c1083d2927b64eca2365600ea8d7492807cd3
sanginjeong <sanginjeong07@gmail.com>
2025-10-08 18:49:34 +0900

    feat: 지원 승인,거절,취소

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { useGetShopApplicationsQuery } from "@/hooks/api/application/useGetShopApplicationsQuery";
 import { usePostShopApplicationQuery } from "@/hooks/api/application/usePostShopApplicationQuery";
+import { usePutShopApplicationQuery } from "@/hooks/api/application/usePutShopApplicationQuery";
 import { useLoginQuery } from "@/hooks/api/user/useLoginQuery";
 
 export default function Home() {
@@ -9,10 +10,22 @@ export default function Home() {
   });
   const { mutate: postShopApplication } = usePostShopApplicationQuery();
   const { mutate: postLogin } = useLoginQuery();
+  const { mutate: putApp } = usePutShopApplicationQuery();
   const tt = () => {
     postShopApplication({
       shopId: "9e39dfd0-c684-443b-8727-c1d1a47344bd",
       noticeId: "fc0aad52-832b-4388-8a8e-129d5160a6b3",
+    });
+  };
+
+  const pp = () => {
+    putApp({
+      shopId: "9e39dfd0-c684-443b-8727-c1d1a47344bd",
+      noticeId: "fc0aad52-832b-4388-8a8e-129d5160a6b3",
+      applicationId: "8acc0718-a154-41f6-a25b-a5edf7d6447c",
+      data: {
+        status: "rejected",
+      },
     });
   };
 
@@ -25,6 +38,7 @@ export default function Home() {
       <h1>3팀 파이팅!</h1>
       <button onClick={ll}>로</button>
       <button onClick={tt}>지원</button>
+      <button onClick={pp}>ddd</button>
     </>
   );
 }
