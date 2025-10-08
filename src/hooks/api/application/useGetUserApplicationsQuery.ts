dd00243e5fc558e83e4825da2302840eb4a835dd
sanginjeong <sanginjeong07@gmail.com>
2025-10-08 18:28:17 +0900

    feat: 유저의 지원 목록 조회

---
+++ src/hooks/api/application/useGetUserApplicationsQuery.ts
@@ -0,0 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
+import { instance } from "@/utils/instance";
+import { ApplicationItem, Link, NoticeItem, ShopItem } from "@/types/global";
+
+export interface GetUserApplicationsRequest {
+  userId: string;
+  params?: {
+    offset?: number;
+    limit?: number;
+  };
+}
+
+export interface GetUserApplicationsResponse {
+  offset: number;
+  limit: number;
+  count: number;
+  hasNext: boolean;
+  items: Array<{
+    items: ApplicationItem & {
+      shop: {
+        item: ShopItem;
+        href: string;
+      };
+      notice: {
+        item: NoticeItem;
+      };
+    };
+    links: Link[];
+  }>;
+  links: Link[];
+}
+
+const getUserApplications = async ({
+  userId,
+  params,
+}: GetUserApplicationsRequest): Promise<GetUserApplicationsResponse> => {
+  const response = await instance.get(`/users/${userId}/applications`, { params });
+  return response.data;
+};
+
+export const useGetUserApplicationsQuery = ({ userId, params }: GetUserApplicationsRequest) => {
+  return useQuery({
+    queryKey: ["getUserApplications", userId, params],
+    queryFn: () => getUserApplications({ userId, params }),
+  });
+};
