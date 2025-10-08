30a19db873011883a5854efb8c5f96d68e7c20fd
sanginjeong <sanginjeong07@gmail.com>
2025-10-08 15:24:56 +0900

    feat: 가게 공고 등록

---
+++ src/hooks/api/notice/usePostShopNoticesQuery.ts
@@ -0,0 +1,44 @@
+import { useMutation } from "@tanstack/react-query";
+import { instance } from "@/utils/instance";
+import { Link, NoticeItem, ShopItem } from "@/types/global";
+
+export interface PostShopNoticesRequest {
+  shopId: string;
+  hourlyPay: number;
+  startsAt: string; //양식: 2023-12-23T00:00:00Z
+  workhour: number;
+  description: string;
+}
+
+export interface PostShopNoticesResponse {
+  item: NoticeItem & {
+    shop: {
+      item: ShopItem;
+      href: string;
+    };
+  };
+  links: Link[];
+}
+
+const postShopNotices = async ({
+  shopId,
+  hourlyPay,
+  startsAt,
+  workhour,
+  description,
+}: PostShopNoticesRequest): Promise<PostShopNoticesResponse> => {
+  const response = await instance.post(`/shops/${shopId}/notices`, { hourlyPay, startsAt, workhour, description });
+  return response.data;
+};
+
+export const usePostShopNoticesQuery = () => {
+  return useMutation({
+    mutationFn: postShopNotices,
+    onSuccess: (res) => {
+      console.log(res);
+    },
+    onError: (err) => {
+      console.log(err);
+    },
+  });
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,34 @@
 import { useGetNoticesQuery } from "@/hooks/api/notice/useGetNoticesQuery";
 import { useGetShopNoticesQuery } from "@/hooks/api/notice/useGetShopNoticesQuery";
+import { usePostShopNoticesQuery } from "@/hooks/api/notice/usePostShopNoticesQuery";
+import { useLoginQuery } from "@/hooks/api/user/useLoginQuery";
 
 export default function Home() {
+  const { mutate: postLogin } = useLoginQuery();
   const { data: notices } = useGetNoticesQuery({});
   const { data: shopNotices } = useGetShopNoticesQuery({ shopId: "9e39dfd0-c684-443b-8727-c1d1a47344bd" });
-  console.log(notices);
-  console.log(shopNotices);
+  const { mutate: postShopNotices } = usePostShopNoticesQuery();
+  // console.log(notices);
+  // console.log(shopNotices);
 
+  const tt = () => {
+    postShopNotices({
+      shopId: "9e39dfd0-c684-443b-8727-c1d1a47344bd",
+      hourlyPay: 12000,
+      startsAt: "2025-12-23T00:00:00Z",
+      workhour: 3,
+      description: "정상인",
+    });
+  };
+
+  const login = () => {
+    postLogin({ email: "test@a.com", password: "1" });
+  };
   return (
     <>
       <h1>3팀 파이팅!</h1>
+      <button onClick={login}>로그인</button>
+      <button onClick={tt}>클릭</button>
     </>
   );
 }
