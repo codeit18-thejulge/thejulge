8c1703dfb8dcad3e3ddf68a7552c7979a96e4e9f
kim-hyunhwa <pinpa@naver.net>
2025-10-09 14:17:55 +0900

    rename:아이콘 이름 변경 chore: 테스트 코드 삭제

--- src/components/Badge/ClosedBadge/index.tsx
+++ src/components/Badge/ClosedBadge/index.tsx
@@ -1,4 +1,4 @@
-import IcXButtonIcon from "@/assets/svgs/ic_x.svg";
+import IcXButton from "@/assets/svgs/ic_x.svg";
 
 interface Props {
   propText: string;
@@ -11,7 +11,7 @@ const ClosedBadge = ({ propText }: Props) => {
     <div className="flex max-w-fit items-center gap-6 rounded-20 bg-red-10 px-10 py-6 text-12-regular leading-none text-primary tablet:text-14-bold">
       <span>{propText}</span>
       <button type="button" onClick={onClickDelete} aria-label="삭제">
-        <IcXButtonIcon />
+        <IcXButton />
       </button>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,9 @@
-import NormalBadge from "@/components/Badge/NormalBadge";
-import PayBadge from "@/components/Badge/PayBadge";
-import ClosedBadge from "@/components/Badge/ClosedBadge";
+import Post from "@/components/Post";
 
 export default function Home() {
   return (
     <>
       <h1>3팀 화이팅</h1>
-      <NormalBadge status="pending"/>
-      <NormalBadge status={"accepted"}/>
-      <NormalBadge status={"rejected"}/>
-      <NormalBadge status={"canceled"}/>
-      <PayBadge hourlyPay={15000} originalHourlyPay={10000} closed={false}/>
-      <ClosedBadge propText={"서울시 성북"} />
     </>
   );
 }
