dbfb216247db7c533b7944e8bd62d6d5a129a2b2
sejin5 <sejin5.dev@gmail.com>
2025-10-10 22:02:14 +0900

    fix: console.log 제거

--- src/components/SelectBox/SelectOptions.tsx
+++ src/components/SelectBox/SelectOptions.tsx
@@ -15,7 +15,7 @@ const SelectOptions = ({ isOpen, options, onSelect, style, className }: OptionPr
   if (!isOpen) {
     return null;
   }
-  console.log(options);
+
   return (
     <div className={cn("custom-scroll absolute left-0 right-0 z-40 my-8 overflow-y-auto", style.boxSelect, className)}>
       {options.map((option, index) => (
