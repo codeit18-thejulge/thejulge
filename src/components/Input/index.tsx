0bf594c5ec2d22153fe957b36f948e200cf5f56a
sejin5 <sejin5.dev@gmail.com>
2025-10-11 14:52:27 +0900

    fix: autoComplete 수정가능하도록 변경

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/utils";
 interface InputProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
   isPay?: boolean;
   errorMsg?: string;
+  autoComplete?: string;
 }
 
 const inputStyle = {
@@ -12,26 +13,28 @@ const inputStyle = {
   pay: "flex justify-between",
 };
 
-const Input = forwardRef<HTMLInputElement, InputProps>(({ isPay, errorMsg, className, ...props }, ref) => {
-  return (
-    <>
-      <div className={cn(inputStyle.basic, errorMsg && inputStyle.error, isPay && inputStyle.pay)}>
-        <input
-          name={props.name}
-          type={props.type}
-          value={props.value}
-          placeholder="입력"
-          className={cn("w-full bg-transparent px-20 py-16 focus:outline-none", isPay && "w-[90%]", className)}
-          ref={ref}
-          {...props}
-          autoComplete="off"
-        />
-        {isPay && <span className="py-16 pr-20 text-16">원</span>}
-      </div>
-      {errorMsg && <p className="mt-8 text-12 text-red-40">{errorMsg}</p>}
-    </>
-  );
-});
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ isPay, errorMsg, className, autoComplete = "off", ...props }, ref) => {
+    return (
+      <>
+        <div className={cn(inputStyle.basic, errorMsg && inputStyle.error, isPay && inputStyle.pay)}>
+          <input
+            name={props.name}
+            type={props.type}
+            value={props.value}
+            placeholder="입력"
+            className={cn("w-full bg-transparent px-20 py-16 focus:outline-none", isPay && "w-[90%]", className)}
+            ref={ref}
+            autoComplete={autoComplete}
+            {...props}
+          />
+          {isPay && <span className="py-16 pr-20 text-16">원</span>}
+        </div>
+        {errorMsg && <p className="mt-8 text-12 text-red-40">{errorMsg}</p>}
+      </>
+    );
+  },
+);
 
 Input.displayName = "Input";
 
