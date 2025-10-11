574b777ba0683eb62c41c36e5727e331b6a9588d
kim-hyunhwa <pinpa@naver.net>
2025-10-11 11:25:43 +0900

    refactor: outBox 클래스 <div> 삭제

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,10 +1,8 @@
 import TableHeader from "./components/TableHeader";
 import TableRow from "./components/TableRow";
-// import ListPagination from "@/components/ListPagination";
 import { useState, useEffect } from "react";
 import { Notice } from "@/types/notice";
 import tableStyle from "@/styles/table.module.css";
-
 
 const TABLE_HEADER = {
   employer: [
@@ -23,59 +21,42 @@ const TABLE_HEADER = {
 
 interface TableProps {
   userType: keyof typeof TABLE_HEADER;
-  // limit: number;
-  // count: number;
-  // hasNext: boolean;
   res: Notice[];
   onHandleRejectClick?: () => void; // 거절 버튼 클릭 시 호출
   onHandleAcceptClick?: () => void; // 승인 버튼 클릭 시 호출
   onPageChange: () => void;
 }
 
-const Table = ({
-  userType,
-  res,
-  // limit=0, count=0, hasNext, onPageChange,
-  onHandleRejectClick,
-  onHandleAcceptClick,
-}: TableProps) => {
+const Table = ({ userType, res, onHandleRejectClick, onHandleAcceptClick }: TableProps) => {
   const headerTitles = TABLE_HEADER[userType];
   const [tableData, setTableData] = useState<Notice[]>([]);
   useEffect(() => {
     setTableData(res);
   }, [res]);
   return (
-    <div className={tableStyle.outBox}>
-      <div className={tableStyle.tableWrap}>
-        <table className={tableStyle.table}>
-          <colgroup>
-            <col className="w-228" />
-            <col className="w-300" />
-            <col className="w-200" />
-            <col className="w-162 tablet:w-220 desktop:w-236" />
-          </colgroup>
-          <thead className={tableStyle.theadColor}>
-            <TableHeader colTitle={headerTitles} />
-          </thead>
-          <tbody>
-            {tableData.map((item) => (
-              <TableRow
-                key={item.id}
-                item={item}
-                userType={userType}
-                onHandleRejectClick={onHandleRejectClick}
-                onHandleAcceptClick={onHandleAcceptClick}
-              />
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <div className={tableStyle.tableBottom}>
-        {/* 페이지 네이션 컴포넌트 만들어지면 오류 없습니다.
-        <ListPagination
-        // limit={limit} count={count} hasNext={hasNext} onPageChange={onPageChange}
-        />*/}
-      </div>
+    <div className={tableStyle.tableWrap}>
+      <table className={tableStyle.table}>
+        <colgroup>
+          <col className="w-228" />
+          <col className="w-300" />
+          <col className="w-200" />
+          <col className="w-162 tablet:w-220 desktop:w-236" />
+        </colgroup>
+        <thead className={tableStyle.theadColor}>
+          <TableHeader colTitle={headerTitles} />
+        </thead>
+        <tbody>
+          {tableData.map((item) => (
+            <TableRow
+              key={item.id}
+              item={item}
+              userType={userType}
+              onHandleRejectClick={onHandleRejectClick}
+              onHandleAcceptClick={onHandleAcceptClick}
+            />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
