d3a0e9e0467145d44d1e69fe2351ed79d44e7f3e
sanginjeong <sanginjeong07@gmail.com>
2025-10-12 22:16:11 +0900

    feat: 등록된 공고가 있을 시에 신청내역 보여주기 구현 (테이블, 페이지네이션  제외)

--- src/pages/profile/(components)/Profile/ProfileDetail/ApplicationLogSection/index.tsx
+++ src/pages/profile/(components)/Profile/ProfileDetail/ApplicationLogSection/index.tsx
@@ -1,11 +1,28 @@
+import LoadingSpinner from "@/components/LoadingSpinner";
+import Table from "@/components/Table";
+import { useGetUserApplicationsQuery } from "@/hooks/api/application/useGetUserApplicationsQuery";
+
 interface Props {
   userId: string;
 }
 
 const ApplicationLogSection = ({ userId }: Props) => {
+  const { data, isLoading, isError, error } = useGetUserApplicationsQuery({ userId });
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
+  if (isError || !data) {
+    return isError ? <p>{error.message}</p> : <p>데이터를 불러오지 못했습니다</p>;
+  }
+
+  const { items } = data;
+
   return (
-    <div className="">
+    <div className="mx-auto flex max-w-6xl flex-col gap-12">
       <h2 className="flex-[1] text-20-bold tablet:text-28-bold">신청내역</h2>
+      {/* <Table userType="employee" res={items} /> */}
     </div>
   );
 };
