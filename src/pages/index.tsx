5c6a2902d850b6770ef7f8e4755d19288a72ece2
sanginjeong <sanginjeong07@gmail.com>
2025-10-13 01:58:41 +0900

    chore: 컴포넌트 폴더들 Landing폴더로 이동

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Footer from "@/components/Footer";
-import LandingHeader from "@/components/Header/LandingHeader";
-import HeroSection from "@/components/HeroSection";
-import FeaturesSection from "@/components/FeaturesSection";
+import LandingHeader from "@/components/Landing/LandingHeader";
+import HeroSection from "@/components/Landing/HeroSection";
+import FeaturesSection from "@/components/Landing/FeaturesSection";
 export default function Home() {
   return (
     <>
