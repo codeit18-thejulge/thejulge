6ec3222150209e49863275c481182189a7b7a31d
sanginjeong <sanginjeong07@gmail.com>
2025-10-13 19:53:05 +0900

    fix: 페이지별 라우터를 getLayout을 활용하는  구조로 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Layout from "@/components/Layout";
-import useLayout from "@/hooks/useLayout";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  const hasLayout = useLayout();
+  const getLayout = Component.getLayout ?? ((page) => page);
 
-  const pageElement = <Component {...pageProps} />;
-  const content = hasLayout ? <Layout>{pageElement}</Layout> : pageElement;
-
-  return <QueryClientProvider client={queryClient}>{content}</QueryClientProvider>;
+  return <QueryClientProvider client={queryClient}>{getLayout(<Component {...pageProps} />)}</QueryClientProvider>;
 }
