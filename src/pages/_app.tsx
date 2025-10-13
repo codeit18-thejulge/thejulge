2e3650f08f3dbf5e3696018d748d8d9260ce07ee
sanginjeong <sanginjeong07@gmail.com>
2025-10-13 19:59:22 +0900

    feat: 레이아웃 타입 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NextPage } from "next";
+import { ReactNode } from "react";
 
 const queryClient = new QueryClient();
 
-export default function App({ Component, pageProps }: AppProps) {
-  const getLayout = Component.getLayout ?? ((page) => page);
+type NextPageWithLayout = NextPage & {
+  getLayout: (page: ReactNode) => ReactNode;
+};
+
+export default function App({ Component, pageProps }: AppProps & { Component: NextPageWithLayout }) {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
 
   return <QueryClientProvider client={queryClient}>{getLayout(<Component {...pageProps} />)}</QueryClientProvider>;
 }
