8d042a49abfadb109e9d4be857562779b6f3261c
minyoung1101 <my11612@naver.com>
2025-10-13 11:22:53 +0900

    refactor: UserHeader 메뉴 텍스트 변수로 분리

--- src/components/Header/UserHeader/index.tsx
+++ src/components/Header/UserHeader/index.tsx
@@ -3,41 +3,63 @@ import Link from "next/link";
 import { UserType } from "@/types/global";
 import { cn } from "@/utils";
 
-const USER_MENU_LABEL_MAP = {
-  employee: "내 프로필",
-  employer: "내 가게",
-};
-
 const linkStyle = "text-14-bold tablet:text-16-bold";
+
+const guestMenuItem = {
+  signin: {
+    title: "로그인",
+    href: "/signin",
+  },
+  signup: {
+    title: "회원가입",
+    href: "/signup",
+  },
+};
+const userMenuItem = {
+  userPage: {
+    employee: {
+      title: "내 프로필",
+      href: "/profile",
+    },
+    employer: {
+      title: "내 가게",
+      href: "/shopinfo",
+    },
+  },
+  logout: {
+    title: "로그아웃",
+    href: "/joblist",
+  },
+};
 
 const GuestMenu = () => {
   return (
     <ul className="flex items-center gap-16 desktop:gap-40">
       <li>
-        <Link href="/" className={cn(linkStyle)}>
-          로그인
+        <Link href={guestMenuItem.signin.href} className={cn(linkStyle)}>
+          {guestMenuItem.signin.title}
         </Link>
       </li>
       <li>
-        <Link href="/" className={cn(linkStyle)}>
-          회원가입
+        <Link href={guestMenuItem.signup.href} className={cn(linkStyle)}>
+          {guestMenuItem.signup.title}
         </Link>
       </li>
     </ul>
   );
 };
 
-const UserMenu = ({ userType, userPage }: { userType: UserType; userPage: string }) => {
+const UserMenu = ({ userType }: { userType: UserType }) => {
   return (
     <ul className="flex items-center gap-16 desktop:gap-40">
       <li>
-        <Link href={userPage} className={cn(linkStyle)}>
-          {USER_MENU_LABEL_MAP[userType]}
+        <Link href={userMenuItem.userPage[userType].href} className={cn(linkStyle)}>
+          {userMenuItem.userPage[userType].title}
         </Link>
       </li>
       <li>
-        <Link href="/" className={cn(linkStyle)}>
-          로그아웃
+        <Link href={userMenuItem.logout.href} className={cn(linkStyle)}>
+          {userMenuItem.logout.title}
         </Link>
       </li>
       <li>
@@ -54,13 +76,11 @@ const UserHeader = () => {
   // 전역으로 관리되는 유저 정보 사용할 예정
   // (아직 어떤 구조가 될 지 몰라 대략적으로 변수로만 테스트 출력)
   const userType: UserType = "employee";
-  const userPage = "/profile";
   const isLogined = true;
 
   return (
     <nav className="order-2 ml-auto flex h-30 shrink-0 tablet:order-3 tablet:h-40">
-      {!isLogined && <GuestMenu />}
-      {isLogined && <UserMenu userType={userType} userPage={userPage} />}
+      {isLogined ? <UserMenu userType={userType} /> : <GuestMenu />}
     </nav>
   );
 };
