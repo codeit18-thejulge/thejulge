7acb3a2faf6b2f8b5194257187b0f0ff1707f199
minyoung1101 <my11612@naver.com>
2025-10-13 13:31:32 +0900

    feat: 렌딩페이지 레이아웃 제외

--- src/hooks/useLayout.ts
+++ src/hooks/useLayout.ts
@@ -3,8 +3,8 @@ import { useRouter } from "next/router";
 const useLayout = () => {
   const router = useRouter();
 
-  const noLayoutRoutes = ["/signup", "/signin"];
-  const hasLayout = !noLayoutRoutes.some((path) => router.pathname.startsWith(path));
+  const noLayoutRoutes = ["/", "/signup", "/signin"];
+  const hasLayout = !noLayoutRoutes.includes(router.pathname);
 
   return hasLayout;
 };
