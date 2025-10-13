f9643f78af601754ee9584436a63cc062ca04935
minyoung1101 <my11612@naver.com>
2025-10-13 10:28:56 +0900

    design: min-height 변경

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex min-h-dvh flex-col">
       <Header />
       <main className="flex-1">{children}</main>
       <Footer />
