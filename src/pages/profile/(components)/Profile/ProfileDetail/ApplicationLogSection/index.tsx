e400518a1eca1dcc7f4afedfa63e80944e546cd8
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-10-15 17:17:28 +0900

    chore: 테스트 코드 제거

--- src/pages/profile/(components)/Profile/ProfileDetail/ApplicationLogSection/index.tsx
+++ src/pages/profile/(components)/Profile/ProfileDetail/ApplicationLogSection/index.tsx
@@ -1,8 +1,6 @@
-import ListPagination from "@/components/ListPagination";
 import LoadingSpinner from "@/components/LoadingSpinner";
 import Table from "@/components/Table";
 import { useGetUserApplicationsQuery } from "@/hooks/api/application/useGetUserApplicationsQuery";
-import { useRouter } from "next/router";
 
 interface Props {
   userId: string;
@@ -10,11 +8,6 @@ interface Props {
 
 const ApplicationLogSection = ({ userId }: Props) => {
   const { data, isLoading, isError, error } = useGetUserApplicationsQuery({ userId });
-  const router = useRouter();
-
-  const handleApplicationClick = (jobId: string) => {
-    router.push(`/jobinfo/${jobId}`);
-  };
 
   if (isLoading) {
     return <LoadingSpinner />;
@@ -29,8 +22,6 @@ const ApplicationLogSection = ({ userId }: Props) => {
   return (
     <div className="mx-auto flex max-w-6xl flex-col gap-12">
       <h2 className="flex-[1] text-20-bold tablet:text-28-bold">신청내역</h2>
-      <Table userType="employee" res={items} handleApplicationClick={handleApplicationClick} />
-      <ListPagination limit={5} count={data.count} hasNext={data.hasNext} />
     </div>
   );
 };
