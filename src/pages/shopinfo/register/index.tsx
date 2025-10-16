0d27cf40fff279a75ebe8713260c4b2522e535ba
minyoung1101 <my11612@naver.com>
2025-10-17 02:28:19 +0900

    feat: 가게 등록 페이지 구현

---
+++ src/pages/shopinfo/register/index.tsx
@@ -0,0 +1,79 @@
+import { useRouter } from "next/router";
+import RegisterForm, { FormData } from "../_components/RegisterForm";
+import { useState } from "react";
+import IcClose from "@/assets/svgs/ic_close.svg";
+import Layout from "@/components/Layout";
+import ModalWrapper, { ModalProps, ModalType, getModalContent } from "../_components/ModalWrapper";
+import { usePostShopQuery } from "@/hooks/api/shop/usePostShopQuery";
+
+const RegisterJobinfo = () => {
+  const router = useRouter();
+  const { mutate: postShop, isPending } = usePostShopQuery();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalData, setModalData] = useState<ModalProps>({
+    message: "",
+    buttons: [],
+  });
+
+  const handleOpenModal = (type: ModalType, message: string, onConfirm: () => void) => {
+    const content = getModalContent({
+      type,
+      message,
+      onConfirm,
+      onClose: () => setIsModalOpen(false),
+    });
+    setModalData(content);
+    setIsModalOpen(true);
+  };
+
+  const handleSubmit = (data: FormData) => {
+    if (!data.category || !data.address1) {
+      return;
+    }
+    postShop(
+      { ...data, category: data.category, address1: data.address1 },
+      {
+        onSuccess: () => {
+          // 공고 상세로 이동
+          handleOpenModal("confirm", "가게 등록이 완료되었습니다.", () => router.replace(`/shopinfo`));
+        },
+        onError: () => {
+          // 가게 정보 상세로 이동
+          handleOpenModal("confirm", "가게 등록에 실패했습니다.", () => router.push(`/shopinfo`));
+        },
+      },
+    );
+  };
+
+  const handleCloseClick = () => {
+    // 가게 정보 상세로 이동
+    handleOpenModal("action", "가게 등록을 취소하시겠습니까?", () => router.push(`/shopinfo`));
+  };
+
+  return (
+    <div className="bg-gray-5">
+      <div className="m-auto max-w-1028 px-12 py-40 tablet:px-32 tablet:py-60">
+        <div className="relative">
+          <IcClose
+            onClick={handleCloseClick}
+            className="absolute right-0 top-0 w-24 hover:cursor-pointer tablet:w-32"
+          />
+          <h1 className="mb-32 text-20-bold text-black tablet:text-28-bold">내 가게</h1>
+          <RegisterForm onSubmit={handleSubmit} isPending={isPending} submitLabel="등록" />
+          <ModalWrapper
+            isOpen={isModalOpen}
+            message={modalData.message}
+            onClose={() => setIsModalOpen(false)}
+            buttons={modalData.buttons}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterJobinfo;
+RegisterJobinfo.getLayout = (page: React.ReactNode) => {
+  return <Layout>{page}</Layout>;
+};
