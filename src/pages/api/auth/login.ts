7fa7f3c59b31bb3b1969c7a69112ee342bac6c99
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-10-16 23:48:19 +0900

    feat:핸들러 구현

---
+++ src/pages/api/auth/login.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+
+const NEXT_PUBLIC_BACKEND_API_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL;
+
+export default async function Handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ status: "405", message: "허용되지 않은 요청입니다" });
+  }
+
+  try {
+    const { email, password } = req.body;
+
+    const { data } = (await axios.post(`${NEXT_PUBLIC_BACKEND_API_URL}/token`, { email, password })).data;
+
+    const { item } = data;
+
+    const { token } = item;
+    const { userId } = item.user.item;
+
+    res.setHeader("Set-Cookie", [
+      `accessToken=${token}; Path=/ HttpOnly; Secure; SameSite=Strict;`,
+      `userId=${userId};  Path=/`,
+    ]);
+
+    return res.status(200).json({
+      status: 200,
+      data,
+    });
+  } catch (error) {
+    res.status(400).json({ status: "Failed", message: "로그인 실패" });
+  }
+}
---
+++ src/pages/test/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Test = () => {
+  return <div>index</div>;
+};
+
+export default Test;
