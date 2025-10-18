59085feeb78b3cbb120dc5a1997f41530a9d04a7
minyoung1101 <my11612@naver.com>
2025-10-18 18:26:09 +0900

    feat: usertype 쿠키 사용으로 변경 및 ssr에서 체크하는 함수 구현

--- src/hooks/useCheckAuth.ts
+++ src/hooks/useCheckAuth.ts
@@ -1,31 +1,38 @@
 // CSR에서 로그인 여부, 유저 타입 체크
 
-import { getCookieValue } from "@/utils/getCookie";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { getCookieValue } from "@/utils/getCookie";
 
-const useCheckAuth = (requiredUserType: string, redirectPath = "/") => {
+const useCheckAuth = (requiredUserType?: string, redirectPath?: string, requireShop: boolean = false) => {
   const router = useRouter();
-  const [userId, setUserId] = useState("");
-  const [userType, setUserType] = useState("");
 
   useEffect(() => {
-    const id = getCookieValue(document.cookie, "userId") || "";
-    const type = getCookieValue(document.cookie, "userType") || "";
-    setUserId(id);
-    setUserType(type);
-  }, []);
+    const cookie = document.cookie;
+    const userId = getCookieValue(cookie, "userId") || null;
+    const userType = getCookieValue(cookie, "userType") || null;
+    const shopId = getCookieValue(cookie, "shopId") || null;
 
-  useEffect(() => {
+    // 로그인 체크
     if (!userId) {
       alert("로그인이 필요한 서비스입니다.");
       router.replace("/signin");
       return;
     }
-    if (userType !== requiredUserType) {
+
+    // 유저 타입 체크
+    if (requiredUserType && userType !== requiredUserType) {
       alert("접근 권한이 없습니다.");
-      router.replace(redirectPath);
+      router.replace(redirectPath || "/joblist");
+      return;
     }
-  }, [redirectPath, requiredUserType, router, userId, userType]);
+
+    // // employer일때 shopId 체크
+    if (requireShop && requiredUserType === "employer" && !shopId) {
+      alert("가게 정보가 필요합니다.");
+      router.replace(redirectPath || "/shopinfo");
+    }
+  }, [requiredUserType, requireShop, redirectPath, router]);
 };
+
 export default useCheckAuth;
