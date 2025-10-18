fb4dc7006b788da0b57e5cd45511881ee3a8aadb
sejin5 <sejin5.dev@gmail.com>
2025-10-18 16:40:07 +0900

    bug: key 추가

--- src/pages/jobinfo/_components/recentlist.tsx
+++ src/pages/jobinfo/_components/recentlist.tsx
@@ -15,7 +15,7 @@ const RecentList = () => {
       <h2 className="mb-32 text-20 font-bold tablet:text-28">최근에 본 공고</h2>
       <div className="grid grid-cols-2 gap-12 desktop:grid-cols-3">
         {jobs.map((job) => (
-          <Link href={`/jobinfo/${job.shopId}/${job.id}`}>
+          <Link href={`/jobinfo/${job.shopId}/${job.id}`} key={job.id}>
             <Post
               name={job.name}
               hourlyPay={job.hourlyPay}
