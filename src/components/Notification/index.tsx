3941ee2048344bd7d2282e2a006ee3fa3c7d5838
minyoung1101 <my11612@naver.com>
2025-10-20 22:51:50 +0900

    feat: 알림 컴포넌트 스켈레톤 UI 적용

--- src/components/Notification/index.tsx
+++ src/components/Notification/index.tsx
@@ -1,12 +1,13 @@
 import IcClose from "@/assets/svgs/ic_close.svg";
 import NotificationItem from "./NotificationItem";
 import LoadingSpinner from "../LoadingSpinner";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { cn } from "@/utils";
 import { GetUserAlertsResponse, useGetUserAlertsQuery } from "@/hooks/api/alert/useGetUserAlertsQuery";
 import { usePutUserAlertsQuery } from "@/hooks/api/alert/usePutUserAlertsQuery";
 import { getCookieValue } from "@/utils/getCookie";
 import { useQueryClient } from "@tanstack/react-query";
+import SkeletonUI from "../Skeleton";
 
 export type UserAlertItem = GetUserAlertsResponse["items"][number]["item"];
 
@@ -19,17 +20,11 @@ const Notification = ({ onClose, className }: NotificationProps) => {
 
   const [userId, setUserId] = useState("");
   const [alerts, setAlerts] = useState<UserAlertItem[]>([]);
+  const [isAllRead, setIsAllRead] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const { data: alertData, isPending, refetch } = useGetUserAlertsQuery({ userId, options: { enabled: !!userId } });
+  const { data: alertData, refetch } = useGetUserAlertsQuery({ userId, options: { enabled: !!userId } });
   const { mutateAsync: putUserAlerts } = usePutUserAlertsQuery();
-
-  const isAllRead = !isPending && alerts.length === 0;
-  const hasUnread = !isPending && alerts.length > 0;
-
-  const getAlerts = useCallback(async () => {
-    const unreadAlerts = alertData?.items?.map((i) => i.item).filter((alert) => !alert.read) ?? [];
-    setAlerts(unreadAlerts);
-  }, [alertData]);
 
   const handleAlertRead = async (alertId: string) => {
     const prevAlerts = [...alerts];
@@ -48,15 +43,41 @@ const Notification = ({ onClose, className }: NotificationProps) => {
     );
   };
   useEffect(() => {
+    setIsAllRead(!isLoading && alerts.length === 0);
+  }, [alerts.length, isLoading]);
+  useEffect(() => {
     const userCookieId = getCookieValue(document.cookie, "userId") || "";
     setUserId(userCookieId);
   }, []);
 
   useEffect(() => {
     if (userId) {
-      refetch().then(getAlerts);
+      refetch().then(() => {
+        const unreadAlerts = alertData?.items?.map((i) => i.item).filter((alert) => !alert.read) ?? [];
+        setAlerts(unreadAlerts);
+        setIsLoading(false);
+      });
     }
-  }, [userId, getAlerts, refetch]);
+  }, [userId, refetch, alertData]);
+
+  if (isLoading) {
+    return (
+      <section
+        className={cn(
+          "flex h-full w-full flex-col gap-16 bg-white px-20 py-40 tablet:h-400 tablet:w-368 tablet:rounded-10 tablet:border tablet:border-gray-30 tablet:py-24 tablet:shadow-[0_2px_8px_var(--gray-30)]",
+          className,
+        )}
+      >
+        <header>
+          <h1 className="text-20-bold">알림</h1>
+        </header>
+
+        <SkeletonUI count={1} boxClassName="h-24 w-240 rounded-md" />
+        <SkeletonUI count={1} boxClassName="w-full h-80 rounded-lg" />
+        <SkeletonUI count={1} boxClassName="w-full h-80 rounded-lg" />
+      </section>
+    );
+  }
 
   return (
     <section
@@ -75,7 +96,7 @@ const Notification = ({ onClose, className }: NotificationProps) => {
 
       <h2 className="text-16-regular text-gray-50">
         {isAllRead && <>모든 알림을 확인했어요</>}
-        {hasUnread && (
+        {!isAllRead && (
           <>
             <span className="text-16-bold text-black">{alerts.length}</span>개의 읽지 않은 알림이 있어요
           </>
@@ -83,7 +104,7 @@ const Notification = ({ onClose, className }: NotificationProps) => {
       </h2>
 
       <ul className="h-full overflow-y-auto">
-        {isPending && <LoadingSpinner />}
+        {isLoading && <LoadingSpinner />}
         {alerts.map((alert) => (
           <li key={alert.id}>
             <NotificationItem alert={alert} onAlertRead={handleAlertRead} />
