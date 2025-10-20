5e14fdfe37aedc94d6e42579a3bc530ca1d1d9d7
kim-hyunhwa <pinpa@naver.net>
2025-10-20 16:10:43 +0900

    design: 페이 배지. 0% 이하일때 안보이도록 수정, 반응형에서 배경색 나오는 문제 수정

--- src/components/Badge/PayBadge/index.tsx
+++ src/components/Badge/PayBadge/index.tsx
@@ -40,37 +40,41 @@ const PayBadge = ({
   };
 
   return (
-    <div
-      className={cn(
-        "relative flex max-w-fit items-center text-12-regular leading-none tablet:text-14-bold",
-        post && closed ? "text-gray-10" : "text-red-40 tablet:text-white",
-        !post && !closed && "text-white tablet:text-white",
-        !post && closed && "hidden",
+    <>
+      {payDifference > 0 && (
+        <div
+          className={cn(
+            "relative flex max-w-fit items-center text-12-regular leading-none tablet:text-14-bold",
+            post && closed ? "text-gray-10" : "text-red-40 tablet:text-white",
+            !post && !closed && "text-white tablet:text-white",
+            !post && closed && "hidden",
+          )}
+          {...props}
+        >
+          <div
+            className={cn(
+              "absolute h-full w-full rounded-20",
+              post && !closed && "hidden tablet:block",
+              post && closed && "hidden tablet:block tablet:bg-gray-20",
+              !post && !closed && "bg-red-40",
+            )}
+            style={{ backgroundColor: closed ? "#e5e4e7" : opacityLevel() }}
+          ></div>
+          <div
+            className={cn(
+              "relative z-10 flex max-w-fit gap-x-4",
+              post ? "px-0 py-0 tablet:px-12 tablet:py-10" : "px-12 py-10",
+              !post && closed && "hidden",
+              post && closed && "text-gray-30 tablet:text-white",
+            )}
+          >
+            <span className={"inline-block"}>{label}</span>
+            <span className="text-12-regular after:content-['%'] tablet:text-14-bold">{payDifference.toFixed(0)}</span>
+            <div className="w-12 tablet:w-14">{icon}</div>
+          </div>
+        </div>
       )}
-      {...props}
-    >
-      <div
-        className={cn(
-          "absolute h-full w-full rounded-20",
-          post && !closed && "bg-white tablet:bg-red-40",
-          post && closed && "bg-white tablet:bg-gray-20",
-          !post && !closed && "bg-red-40",
-        )}
-        style={{ backgroundColor: opacityLevel() }}
-      ></div>
-      <div
-        className={cn(
-          "relative z-10 flex max-w-fit gap-x-4",
-          post ? "px-0 py-0 tablet:px-12 tablet:py-10" : "px-12 py-10",
-          !post && closed && "hidden",
-          post && closed && "tablet:text-white",
-        )}
-      >
-        <span className={"inline-block"}>{label}</span>
-        <span className="text-12-regular after:content-['%'] tablet:text-14-bold">{payDifference.toFixed(0)}</span>
-        <div className="w-12 tablet:w-14">{icon}</div>
-      </div>
-    </div>
+    </>
   );
 };
 
