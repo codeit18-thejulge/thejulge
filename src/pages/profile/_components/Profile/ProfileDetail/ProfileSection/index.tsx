a34d020e45a48f27590cd3adafa9189b1f4ef480
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-10-20 17:51:16 +0900

    chore: 프로필 페이지 색상 수정

--- src/pages/profile/_components/Profile/ProfileDetail/ProfileSection/index.tsx
+++ src/pages/profile/_components/Profile/ProfileDetail/ProfileSection/index.tsx
@@ -33,16 +33,16 @@ const ProfileSection = ({ userId }: Props) => {
   return (
     <div className="mx-auto flex max-w-5xl flex-col gap-24 px-24 py-60 desktop:flex-row">
       <h2 className="flex-[1] text-20-bold tablet:text-28-bold">내 프로필</h2>
-      <section className="flex flex-[2] justify-between rounded-xl bg-red-10 p-32">
+      <section className="bg-third flex flex-[2] justify-between rounded-xl p-32">
         <div className="flex flex-col gap-12">
           <p className="text-16-bold text-primary">이름</p>
           <p className="text-28-bold">{item.name}</p>
           <div className="flex items-center gap-8">
-            <IcPhone className="h-20 w-20 text-red-30" />
+            <IcPhone className="h-20 w-20 text-primary" />
             <p className="text-16-regular text-gray-50">{item.phone}</p>
           </div>
           <div className="flex items-center gap-8">
-            <IcAddress className="h-20 w-20 text-red-30" />
+            <IcAddress className="h-20 w-20 text-primary" />
             <p className="text-16-regular text-gray-50">선호 지역: {item.address}</p>
           </div>
           <p className="mt-24 text-16-regular">{item.bio}</p>
